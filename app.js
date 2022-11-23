0a76285f05862827539f62f2b988b741c9f6c2d6
emilytwines <emdelbridge@gmail.com>
2022-11-23 16:22:52 -0500

    adding grid, in process

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 
 
-//create username
-let username = prompt("Choose a username.");
+// //create username
+// let username = prompt("Choose a username.");
 
-//create a password
-let userPass = prompt("Please enter a password.");
+// //create a password
+// let userPass = prompt("Please enter a password.");
 
 //login
 
-let correctLogin = false; 
-let userLoginName = prompt("Please enter your username.");
-while (correctLogin == false){
-    while (userLoginName != username){
-        userLoginName = prompt("We don't have an account associated with that username! \nPlease try again.");
-    }
-    let userLoginPass = prompt("Please enter your password.");
-    while (userLoginPass != userPass){
-        userLoginPass = prompt("Incorrect password. \nPlease try again.")
-    }
-    correctLogin = true; 
-}
+// let correctLogin = false; 
+// let userLoginName = prompt("Please enter your username.");
+// while (correctLogin == false){
+//     while (userLoginName != username){
+//         userLoginName = prompt("We don't have an account associated with that username! \nPlease try again.");
+//     }
+//     let userLoginPass = prompt("Please enter your password.");
+//     while (userLoginPass != userPass){
+//         userLoginPass = prompt("Incorrect password. \nPlease try again.")
+//     }
+//     correctLogin = true; 
+// }
+
 
