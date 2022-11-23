22a7cbbf3031c5c45eb947ee8351b9bfc5bf8df1
emilytwines <emdelbridge@gmail.com>
2022-11-23 17:55:39 -0500

    EOD, working on click events

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ATM
+Build a basic ATM app that allows a user to:
+
+Create a username
+Create a password
+Log in with their username/password (re-prompting them if they enter the wrong credentials)
+See a menu with options to:
+
+view their balance (this can be hardcoded)
+change their username or password
+deposit funds (add to current balance)
+withdraw funds (remove funds from current balance)
+logout
+Bonus:
+Create an actual frontend for this app inside of index.html :eyes:
--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 
+addEventListener;
 
 // //create username
-// let username = prompt("Choose a username.");
+let username = prompt("Choose a username.");
 
 // //create a password
-// let userPass = prompt("Please enter a password.");
+let userPass = prompt("Please enter a password.");
 
 //login
 
-// let correctLogin = false; 
-// let userLoginName = prompt("Please enter your username.");
-// while (correctLogin == false){
-//     while (userLoginName != username){
-//         userLoginName = prompt("We don't have an account associated with that username! \nPlease try again.");
-//     }
-//     let userLoginPass = prompt("Please enter your password.");
-//     while (userLoginPass != userPass){
-//         userLoginPass = prompt("Incorrect password. \nPlease try again.")
-//     }
-//     correctLogin = true; 
-// }
+let correctLogin = false; 
+let userLoginName = prompt("Please enter your username.");
+while (correctLogin == false){
+    while (userLoginName != username){
+        userLoginName = prompt("We don't have an account associated with that username! \nPlease try again.");
+    }
+    let userLoginPass = prompt("Please enter your password.");
+    while (userLoginPass != userPass){
+        userLoginPass = prompt("Incorrect password. \nPlease try again.");
+    }
+    correctLogin = true; 
+}
 
+
+
+
+let blnce = document.getElementById('balance');
+blnce && blnce.addEventListener('click', balance);
+
+
+function balance(){
+    alert( '$100');
+}
+
+function deposit(){
+    let deposit = prompt("How much would you like to deposit?");
+    alert(deposit);
+}
 
