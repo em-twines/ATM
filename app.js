e4030750aae79665c44c23fcf6dc0f07c99e0025
emilytwines <emdelbridge@gmail.com>
2022-11-24 23:20:17 -0500

    added responsive capacity + functions through withdraw

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 
 addEventListener;
 
+let money = 100; 
+
+
 // //create username
 let username = prompt("Choose a username.");
 
@@ -24,17 +27,57 @@ while (correctLogin == false){
 
 
 
-
-let blnce = document.getElementById('balance');
-blnce && blnce.addEventListener('click', balance);
-
-
-function balance(){
-    alert( '$100');
+const blnc = document.getElementById('balance');
+// let btn = document.addEventListener('click', balance);
+if (blnc) {
+    blnc.addEventListener('click', () => {
+      balance();
+      console.log('btn clicked');
+    })
 }
+
+
+// function getDeposit(){
+//     document.getElementById('deposit');
+//     console.log('balance clicked');
+
+// }
+function balance(){
+    alert(money);
+}
+
+const dep = document.getElementById('deposit');
+// let btn = document.addEventListener('click', balance);
+if (dep) {
+    dep.addEventListener('click', () => {
+      deposit();
+      console.log('deposit clicked');
+    })
+}
+
 
 function deposit(){
     let deposit = prompt("How much would you like to deposit?");
-    alert(deposit);
+    let depositInt = parseInt(deposit);
+    money = money + depositInt; 
+    alert(money);
+    // document.getElementById("deposit").innerHTML = money.deposit;
 }
 
+
+const withd = document.getElementById('withdraw');
+// let btn = document.addEventListener('click', balance);
+if (withd) {
+    withd.addEventListener('click', () => {
+      withdraw();
+      console.log('withdraw clicked');
+    })
+}
+
+function withdraw(){
+    let withdraw = prompt("How much would you like to withdraw?");
+    let withdrawtInt = parseInt(withdraw);
+    money = money - withdrawtInt; 
+    alert(money);
+
+}
